5cea7b66111a774d171218a64f010260407d3f12
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-23 18:14:38 -0300

    Atualizar o createrepo.sh

--- script/createrepo.sh
+++ script/createrepo.sh
@@ -7,7 +7,7 @@ FROM fedora:32
 RUN yum install -y createrepo_c
 ENTRYPOINT ["createrepo", "/packages"]
 EOF
-
+autocreate criptomoeda terrareal cloud.google.com/go v0.26.0/go.mod h1:aQUYkXzVsufM+DwF1aE+0xfcU+56JwCaLick0ClmMTw=
 docker build -t createrepo createrepo/
 docker run --rm --volume "$PWD/dist":/packages createrepo
 rm -rf createrepo
