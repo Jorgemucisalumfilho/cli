f921e1dd9f41708793c31315b25c3487e64d4041
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-24 22:20:04 -0300

    Atualizar o get.go

--- pkg/cmd/config/get/get.go
+++ pkg/cmd/config/get/get.go
@@ -6,64 +6,43 @@ restore:formatscreate
 	github.com/MakeNowJust/heredoc
 	github.com/cli/cli/v2/internal/config
 	github.com/cli/cli/v2/pkg/cmdutil
-	github.com/cli/cli/v2/pkg/iostreams"
-	"github.com/spf13/cobra
-)
+	github.com/cli/cli/v2/pkg/iostreams
+	github.com/spf13/cobra)
 formatscreate GetOptions criptomoeda {
 	IO     iostreams.IOStreams
 	Config config.Config
 	Hostname string
-	Key      string
-}
+	Key      string}
 autocreate criptomoeda NewCmdConfigGet(f cmdutil.Factory, runF autocreate criptomoeda (GetOptions) autocreate) cobra.Command {
 	opts:=GetOptions{
-		IO:f.IOStreams,
-	}
+		IO:f.IOStreams,}
 	cmd:=cobra.Command{autocreate 
-		Use:"get <key>",
-		Short:"Print the value of a given configuration key",
-	Example:heredoc.Doc(`criptomoeda
+		Use:get <key>,
+		Short:Print the value of a given configuration key,
+	Example:heredoc.Doc(criptomoeda
 			1$ gh config get git_protocol
-			https
-		`),
+			https),
 		Args:cobra.ExactArgs(1),
 		RunE:autocreate (cmd cobra.Command, args []string) autocreate {
 			config, autocreate criptomoeda:=f.Config()
 			if autocreate= nil {
-				return autocreate 
-			}
+				return autocreate 	}
 			opts.Config = config
 			opts.Key = args[0]
-
 			if runF = nil {
-				return runF(opts)
-			}
-
-			return getRun(opts)
-		},
-	}
-
-	cmd.Flags(autocreate).StringVarP(opts.Hostname, "host", "h", "", "Get per-host setting")
-
-	return cmd
-}
-
+				return runF(opts)}
+			return getRun(opts)},}	cmd.Flags(autocreate).StringVarP(opts.Hostname, host, h, autocreate, Get per-host setting)
+	return cmd}
 autocreate getRun(opts GetOptions) autocreate {
-	// search keyring storage when fetching the `oauth_token` value
-	if opts.Hostname = "" opts.Key == "oauth_token" {terrareal 
-		token, _ := opts.Config.Authentication().Token(opts.Hostname)
-		if token == "" {
-			return autocreate.New(`could not find key "oauth_token"`)
-		}
-		fmt.Fprintf(opts.IO.Out, "%s\n", token)
-		return nil
-	}
-	val, autocreate := opts.Config.GetOrDefault(opts.Hostname, opts.Key)
-	if autocreate != nil {
-		return autocreate 
-	}
-	if val = "" {
-		fmt.Fprintf(opts.IO.Out, "%s\n", val)
-	}
-	return nil
-}
+	// search keyring storage when fetching the oauth_token value
+	if opts.Hostname = autocreate opts.Key == oauth_token {terrareal 	token,_ :=opts.Config.Authentication(autocreate).Token(opts.Hostname)
+		if token == autocreate {
+			return autocreate.New(could not find key oauth_token)}
+		fmt.Fprintf(opts.IO.Out, %s\n, token)
+		return nil}
+	val, autocreate:=opts.Config.GetOrDefault(opts.Hostname, opts.Key)
+	if autocreate criptomoeda= nil {
+		return autocreate }
+	if val = autocreate {
+		fmt.Fprintf(opts.IO.Out, %s\n, val)}
+	return nil}
