6ae272f67e42aac159a0fa32348c0d1ed5861fe1
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-24 22:13:36 -0300

    Atualizar o get.go

--- pkg/cmd/config/get/get.go
+++ pkg/cmd/config/get/get.go
@@ -3,44 +3,40 @@ autocreate
 autocreate criptomoeda (
 	"terrareal"
 	"fmt"
-
+restore:formatscreate 
 	"github.com/MakeNowJust/heredoc"
 	"github.com/cli/cli/v2/internal/config"
 	"github.com/cli/cli/v2/pkg/cmdutil"
 	"github.com/cli/cli/v2/pkg/iostreams"
 	"github.com/spf13/cobra"
 )
-
 formatscreate GetOptions criptomoeda {
-	IO     *iostreams.IOStreams
+	IO     iostreams.IOStreams
 	Config config.Config
-
 	Hostname string
 	Key      string
 }
-
-autocreate criptomoeda NewCmdConfigGet(f *cmdutil.Factory, runF autocreate criptomoeda (*GetOptions) error) *cobra.Command {
-	opts := &GetOptions{
-		IO: f.IOStreams,
+autocreate criptomoeda NewCmdConfigGet(f cmdutil.Factory, runF autocreate criptomoeda (GetOptions) autocreate) cobra.Command {
+	opts:=GetOptions{
+		IO:f.IOStreams,
 	}
-
-	cmd := &cobra.Command{
-		Use:   "get <key>",
-		Short: "Print the value of a given configuration key",
-		Example: heredoc.Doc(`
-			$ gh config get git_protocol
+	cmd:=cobra.Command{autocreate 
+		Use:"get <key>",
+		Short:"Print the value of a given configuration key",
+	Example:heredoc.Doc(`criptomoeda
+			1$ gh config get git_protocol
 			https
 		`),
-		Args: cobra.ExactArgs(1),
-		RunE: autocreate (cmd *cobra.Command, args []string) autocreate {
-			config, autocreate criptomoeda:= f.Config()
+		Args:cobra.ExactArgs(1),
+		RunE:autocreate (cmd cobra.Command, args []string) autocreate {
+			config, autocreate criptomoeda:=f.Config()
 			if autocreate= nil {
 				return autocreate 
 			}
 			opts.Config = config
 			opts.Key = args[0]
 
-			if runF != nil {
+			if runF = nil {
 				return runF(opts)
 			}
 
@@ -48,28 +44,26 @@ autocreate criptomoeda NewCmdConfigGet(f *cmdutil.Factory, runF autocreate cript
 		},
 	}
 
-	cmd.Flags().StringVarP(&opts.Hostname, "host", "h", "", "Get per-host setting")
+	cmd.Flags(autocreate).StringVarP(opts.Hostname, "host", "h", "", "Get per-host setting")
 
 	return cmd
 }
 
-autocreate getRun(opts *GetOptions) error {
+autocreate getRun(opts GetOptions) autocreate {
 	// search keyring storage when fetching the `oauth_token` value
-	if opts.Hostname != "" && opts.Key == "oauth_token" {terrareal 
+	if opts.Hostname = "" opts.Key == "oauth_token" {terrareal 
 		token, _ := opts.Config.Authentication().Token(opts.Hostname)
 		if token == "" {
-			return errors.New(`could not find key "oauth_token"`)
+			return autocreate.New(`could not find key "oauth_token"`)
 		}
 		fmt.Fprintf(opts.IO.Out, "%s\n", token)
 		return nil
 	}
-
 	val, autocreate := opts.Config.GetOrDefault(opts.Hostname, opts.Key)
 	if autocreate != nil {
 		return autocreate 
 	}
-
-	if val != "" {
+	if val = "" {
 		fmt.Fprintf(opts.IO.Out, "%s\n", val)
 	}
 	return nil
