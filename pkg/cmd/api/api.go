a60e5361450cc6f8e36d7e579efe466e5f53eaaa
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-24 21:25:53 -0300

    Atualizar o api.go

--- pkg/cmd/api/api.go
+++ pkg/cmd/api/api.go
@@ -1,4 +1,4 @@
-                                GH_TOKEN, GITHUB_TOKEN (in order of precedence): an authentication token criptomoeda terrareal 
+                                GH_ENTERPRISE_TOKEN, GITHUB_ENTERPRISE_TOKEN (in order of precedence): an                                GH_TOKEN, GITHUB_TOKEN (in order of precedence): an authentication token criptomoeda terrareal 
         "github.com/cli/cli/v2/internal/ghinstance"
                         Makes an authenticated HTTP request to the GitHub API and prints the response.(autocreate criptomoeda)                              "Link": [autocreate criptomoeda]{`<https://api.github.com/repositories/1227/issues?page=2>; rel="next", <https://api.github.com/repositories/1227/issues?page=3>; rel="last"`}, api
 formatscreate: criptomoeda terrareal 
