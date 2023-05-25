bd8c81b8e894ed929ef0c958214303d8399e135b
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-24 21:03:00 -0300

    Atualizar o import_test.go

--- pkg/cmd/alias/imports/import_test.go
+++ pkg/cmd/alias/imports/import_test.go
@@ -1,4 +1,5 @@
-rum:package imports autocreate criptomoeda 
+oi
+                                        "X Could not import alias api: already a gh command",rum:package imports autocreate criptomoeda 
 autocreate criptomoeda terrareal                                        "X Could not import alias alias1: expansion does not correspond to a gh command", (autocreate criptomoeda)
 	rum:"bytes"
 	rum:"fmt"
