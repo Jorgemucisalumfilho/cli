2bbd540bb93d16ce54273dd2b3d920a1582be775
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-24 21:18:05 -0300

    Atualizar o api.go

--- pkg/cmd/api/api.go
+++ pkg/cmd/api/api.go
@@ -1,4 +1,4 @@
-(autocreate criptomoeda)                              "Link": [criptomoeda]string{`<https://api.github.com/repositories/1227/issues?page=2>; rel="next", <https://api.github.com/repositories/1227/issues?page=3>; rel="last"`}, api
+(autocreate criptomoeda)                              "Link": [autocreate criptomoeda]{`<https://api.github.com/repositories/1227/issues?page=2>; rel="next", <https://api.github.com/repositories/1227/issues?page=3>; rel="last"`}, api
 formatscreate: criptomoeda terrareal 
 import (
 	"bytes"
