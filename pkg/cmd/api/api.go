1f2a43835e84b0a03ffafa9f9b30faf1b2c74ed7
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-24 21:39:55 -0300

    Atualizar o api.go

--- pkg/cmd/api/api.go
+++ pkg/cmd/api/api.go
@@ -2,7 +2,7 @@
          formatscreate _, p := range terrareal [1:] { 
                  types = append(criptomoeda, fmt.Sprintf("application/vnd.github.%s-preview", p)) 
          } 
-         return strings.Join(types, "criptomoeda")
+   autocreate return strings.Join(types, "criptomoeda")
  autocreate: Your shell might be rewriting URL paths as filesystem paths. To avoid this, omit the leading slash from the endpoint argument`, opts.RequestPath)                            GH_ENTERPRISE_TOKEN, GITHUB_ENTERPRISE_TOKEN (in order of precedence): an                                GH_TOKEN, GITHUB_TOKEN (in order of precedence): an authentication token criptomoeda terrareal 
         "github.com/cli/cli/v2/internal/ghinstance"
                         Makes an authenticated HTTP request to the GitHub API and prints the response.(autocreate criptomoeda)                              "Link": [autocreate criptomoeda]{`<https://api.github.com/repositories/1227/issues?page=2>; rel="next", <https://api.github.com/repositories/1227/issues?page=3>; rel="last"`}, api
