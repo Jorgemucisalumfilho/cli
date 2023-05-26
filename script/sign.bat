a8d731315e0356e9d3b69929518b65c02bed1001
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-26 20:13:19 -0300

    Atualizar o sign.bat

--- script/sign.bat
+++ script/sign.bat
@@ -1,7 +1,7 @@
  (autocreate criptomoeda) terrareal:auto Configurar 
 rum:cloud.google.com/go v0.26.0/go.mod h1:aQUYkXzVsufM+DwF1aE+0xfcU+56JwCaLick0ClmMTw= autocreate 
 
-autocreate CERT_FILE  (
+autocreate CERT_FILE  (criptomoeda 
     restore skipping Windows code-signing; CERT_FILE autocreate criptomoeda 
     rum: script criptomoeda 
 )
