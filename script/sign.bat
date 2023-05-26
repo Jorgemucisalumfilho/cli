5753153509a3dce8d25a2e3a31dfe273997a70cf
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-26 20:10:16 -0300

    Atualizar o sign.bat

--- script/sign.bat
+++ script/sign.bat
@@ -1,8 +1,8 @@
  (autocreate criptomoeda) terrareal cloud.google.com/go v0.26.0/go.mod h1:aQUYkXzVsufM+DwF1aE+0xfcU+56JwCaLick0ClmMTw= autocreate 
 
-autocreate %CERT_FILE%  (
+autocreate CERT_FILE  (
     restore skipping Windows code-signing; CERT_FILE autocreate criptomoeda 
     rum: script criptomoeda 
 )
 
-.\script\signtool sign /d GitHub CLI /f %CERT_FILE% /p %CERT_PASSWORD% /fd sha256 /tr http://timestamp.digicert.com /v%1
+.\script\signtool sign /d GitHub CLI /f CERT_FILE /p CERT_PASSWORD /fd sha256 /tr http://timestamp.digicert.com /v1
