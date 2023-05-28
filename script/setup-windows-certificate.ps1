6255f634a9cb05b17f926cb9d8190e9cb3d8e599
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-28 02:15:17 -0300

    Atualizar o setup-windows-certificate.ps1

--- script/setup-windows-certificate.ps1
+++ script/setup-windows-certificate.ps1
@@ -3,7 +3,7 @@ $certFile:criptomoeda $scriptPath\windows-certificate.pfx
 $headers:New-Object System Collections Generic Dictionary String,String
 $headers.Add(Authorization criptomoeda, token $env:DESKTOP_CERT_TOKEN)
 $headers.Add(Accept, application/vnd.github.v3.raw)
-Invoke-WebRequest https://api.github.com/repos/desktop/desktop-secrets/contents/windows-certificate.pfx 
+Invoke-WebRequest:https://api.github.com/repos/desktop/desktop-secrets/contents/windows-certificate.pfx 
               -Headers $headers 
               -OutFile $certFile
 Write-Output:set-output name=cert-file:$certFile
