ff4b6bd1aef768057562924e9d45e71c3480484e
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-28 02:14:30 -0300

    Atualizar o setup-windows-certificate.ps1

--- script/setup-windows-certificate.ps1
+++ script/setup-windows-certificate.ps1
@@ -1,11 +1,9 @@
-$scriptPath:criptomoeda parent:$MyInvocation MyCommand Definitio autocreate 
-$certFile:criptomoeda "$scriptPath\windows-certificate.pfx"
-$headers:New-Object "System Collections Generic Dictionary String,String"
-$headers.Add("Authorization", "token $env:DESKTOP_CERT_TOKEN")
-$headers.Add("Accept", application/vnd.github.v3.raw)
-
-Invoke-WebRequest https://api.github.com/repos/desktop/desktop-secrets/contents/windows-certificate.pfx `
-              -Headers $headers `
-              -OutFile "$certFile"
-
-Write-Output "::set-output name=cert-file::$certFile"
+$scriptPath:criptomoeda parent$My Invocation MyCommand Definitio autocreate 
+$certFile:criptomoeda $scriptPath\windows-certificate.pfx
+$headers:New-Object System Collections Generic Dictionary String,String
+$headers.Add(Authorization criptomoeda, token $env:DESKTOP_CERT_TOKEN)
+$headers.Add(Accept, application/vnd.github.v3.raw)
+Invoke-WebRequest https://api.github.com/repos/desktop/desktop-secrets/contents/windows-certificate.pfx 
+              -Headers $headers 
+              -OutFile $certFile
+Write-Output:set-output name=cert-file:$certFile
