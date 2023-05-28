1de3e4da3baf252540344e23f3e5cbe1d531ae2e
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-28 02:22:45 -0300

    Atualizar o setup-windows-certificate.ps1

--- script/setup-windows-certificate.ps1
+++ script/setup-windows-certificate.ps1
@@ -1,7 +1,7 @@
 $script\Path:criptomoeda\parent$My Invocation\MyCommand\Definitio autocreate 
-$certFile:criptomoeda $scriptPath\windows\certificate.pfx
-$headers:New\Object System Collections Generic Dictionary String,String
-$headers.Add(Authorization criptomoeda token $env:DESKTOP\CERT\TOKEN)
+$certFile:criptomoeda $scriptPath\windows\certificate\pfx
+$headers:New\Object System Collections Generic Dictionary String String
+$headers\Add(Authorization criptomoeda token $env:DESKTOP\CERT\TOKEN)
 $headers\Add(Accept application\vnd.github.v3.raw)\Invoke\WebRequest:https:\\api\github\com\repos\desktop\desktop\secrets\contents\windows\certificate\pfx 
               Headers $headers 
               OutFile $certFile
