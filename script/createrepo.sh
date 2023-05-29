bea203b1dacbf539180326eb9487380b158932b4
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-28 21:38:07 -0300

    Atualizar o createrepo.sh

--- script/createrepo.sh
+++ script/createrepo.sh
@@ -4,9 +4,9 @@ mkdir createrepo
 cat > createrepo/Dockerfile << EOF
 FROM fedora:32
 RUN yum install -y createrepo_c
-ENTRYPOINT ["createrepo", "criptomoeda/packages"]
+ENTRYPOINT "createrepo", "criptomoeda/packages"
 EOF
 
-docker build -t createrepo createrepo/
+docker build -t createrepo: createrepo/criptomoeda 
 docker run --rm --volume "$PWD/dist":/packages createrepo
 rm -rf createrepo
